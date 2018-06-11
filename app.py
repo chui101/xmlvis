bf52ce24685495ed3dfe500f0f0095887b258747
Roger Chui <chui101@gmail.com>
2018-06-10 22:20:15 -0400

    Implement remaining endpoints

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_cors import CORS
 import json
+import kaplanmeier
+import csv
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -16,22 +18,37 @@ def root():
     data = {"case_count": count, "success": True}
     return json.dumps(data)
 
+def state_to_fips(state):
+    with open("statefips.csv",mode='r') as infile:
+        reader = csv.DictReader(infile)
+        for row in reader:
+            if row['usps'] == state:
+                return row['fips']
+
+def get_dbfilter_from_request():
+    dbfilter = request.values.get("dbfilter")
+    if dbfilter is not None:
+        dbfilter = json.loads(dbfilter)
+    return dbfilter
+
+def get_groupings_from_db(field_name, dbfilter = None):
+    query = []
+    if dbfilter is not None:
+        query.append({'$match': dbfilter})
+    query.append({'$group': {'_id': '$' + field_name, 'count': {'$sum': 1}}})
+    result = collection.aggregate(query)
+    return result
+
 @app.route('/counts/',methods=['GET','POST'])
-def getcounts():
-    filter = request.values.get("filter")
-    print(filter)
+def get_counts():
+    dbfilter = get_dbfilter_from_request()
     response = {}
-    if filter != None:
-        filter = json.loads(filter)
-        response['filter'] = filter
+    if dbfilter is not None:
+        response['dbfilter'] = dbfilter
     response['data'] = []
     things_to_count = ['sex','primarySite','vitalStatus','ageAtDiagnosis','race1']
     for groupby in things_to_count:
-        query = []
-        if filter != None:
-            query.append({'$match': filter})
-        query.append({'$group': {'_id':'$'+ groupby, 'count':{'$sum':1} } })
-        result = collection.aggregate(query)
+        result = get_groupings_from_db(groupby, dbfilter)
         if groupby == 'ageAtDiagnosis':
             under18 = 0
             between18and65 = 0
@@ -55,19 +72,94 @@ def getcounts():
     return json.dumps(response)
 
 @app.route('/charts/bar',methods=['GET','POST'])
-def getsitegroupings():
+def get_site_groupings():
     response = []
-    query = []
-    filter = request.values.get("filter")
-    if filter != None:
-        filter = json.loads(filter)
-        query.append({'$match': filter})
-
-    query.append({'$group': {'_id': '$primarySite', 'count': {'$sum': 1}}})
-    result = collection.aggregate(query)
+    dbfilter = get_dbfilter_from_request()
+    result = get_groupings_from_db('primarySite',dbfilter)
     for row in result:
         response.append({'site':row['_id'], 'count':row['count']})
     return json.dumps(response)
+
+
+@app.route('/charts/pie', methods=['GET', 'POST'])
+def get_sex_counts():
+    response = [0,0]
+    dbfilter = get_dbfilter_from_request()
+    result = get_groupings_from_db('sex',dbfilter)
+    for row in result:
+        if row['_id'] == "1":
+            response[1] = row['count']
+        if row['_id'] == "2":
+            response[0] = row['count']
+    return json.dumps(response)
+
+
+
+@app.route('/charts/map',methods=['GET','POST'])
+def get_geo_data():
+    response = []
+    dbfilter = get_dbfilter_from_request()
+    result = get_groupings_from_db('addrAtDxState',dbfilter)
+    for row in result:
+        response.append({'state':row['_id'], 'count':row['count']})
+    return json.dumps(response)
+
+
+@app.route('/charts/map/<state>',methods=['GET', 'POST'])
+def get_geo_data_by_state(state):
+    response = []
+    state = state.upper()
+
+    dbfilter = get_dbfilter_from_request()
+    if dbfilter is None:
+        dbfilter = {"addrAtDxState": state}
+    elif '$and' in dbfilter:
+        dbfilter['$and'].append({"addrAtDxState": state})
+    else:
+        dbfilter = {'$and':[dbfilter,{"addrAtDxState": state}]}
+
+    print(dbfilter)
+    result = get_groupings_from_db('countyAtDx', dbfilter)
+    for row in result:
+        print(row)
+        fips_county = str(state_to_fips(state)) + str(row['_id'])
+        response.append({'county': fips_county, 'count':row['count']})
+    return json.dumps(response)
+
+
+@app.route('/charts/survival', methods=['GET', 'POST'])
+def get_kaplan_meier_by_stage():
+    response = {}
+    response['treatments'] = []
+    dbfilter = get_dbfilter_from_request()
+    for stage in ['1','2','3','4']:
+        if dbfilter is not None:
+            stage_dbfilter = {'$and':[dbfilter, {"majorStageGrp":stage}]}
+        else:
+            stage_dbfilter = {"majorStageGrp":stage}
+        result = collection.find(stage_dbfilter)
+        km = kaplanmeier.kaplan_meier()
+        count = 0
+        for row in result:
+            count += 1
+            km.readFromJson(row)
+        if count > 0:
+            km.buildKaplanMeier(count)
+            response['treatments'].append(km.buildJson())
+        result.close()
+    return json.dumps(response)
+
+
+@app.route('/export/', methods=['GET', 'POST'])
+def export_data_to_json():
+    response = []
+    dbfilter = get_dbfilter_from_request()
+    result = collection.find(dbfilter)
+    for row in result:
+        del row['_id']
+        response.append(row)
+    return json.dumps(response)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- kaplanmeier.py
+++ kaplanmeier.py
@@ -1,20 +1,7 @@
 import json
 from datetime import datetime
 
-jsonstring =[
-
-    { "_id" : "5b1d4fd78e5651446c445449", "patientIdNumber" : 935, "dateOfDiagnosis" : 20110710, "addrAtDxState" : "AR", "derivedAjcc6StageGrp" : "99", "sex" : "2", "race1" : "99", "dateOfLastContact" : 20161104, "lengthOfFollowup" : 1944, "primarySite" : "C51", "ageAtDiagnosis" : 89, "derivedAjcc7StageGrp" : 999, "vitalStatus" : "1" },
-
-    { "_id" : "5b1d4fd78e5651446c4454c7", "patientIdNumber" : 1061, "dateOfDiagnosis" : 20110318, "addrAtDxState" : "MO", "derivedAjcc6StageGrp" : "01", "lengthOfFollowup" : 0, "sex" : "1", "race1" : "99", "dateOfLastContact" : 20110318, "majorStageGrp" : "0", "primarySite" : "C67", "ageAtDiagnosis" : 50, "derivedAjcc7StageGrp" : 10, "vitalStatus" : "1" },
-
-    { "_id" : "5b1d4fd78e5651446c4455ce", "patientIdNumber" : 1324, "dateOfDiagnosis" : 20120130, "addrAtDxState" : "TX", "derivedAjcc6StageGrp" : "01", "lengthOfFollowup" : 0, "sex" : "1", "race1" : "96", "dateOfLastContact" : 20120130, "majorStageGrp" : "0", "primarySite" : "C67", "ageAtDiagnosis" : 72, "derivedAjcc7StageGrp" : 10, "vitalStatus" : "1" },
-
-    { "_id" : "5b1d4fd88e5651446c445634", "patientIdNumber" : 1426, "dateOfDiagnosis" : 20100306, "addrAtDxState" : "NY", "derivedAjcc6StageGrp" : "01", "lengthOfFollowup" : 0, "sex" : "2", "race1" : "99", "dateOfLastContact" : 20100306, "majorStageGrp" : "0", "primarySite" : "C67", "ageAtDiagnosis" : 70, "derivedAjcc7StageGrp" : 10, "vitalStatus" : "1" }
-]
-
-
-
-class kaplanMeier:
+class kaplan_meier:
 
     def __init__(self):
         self.kmc = []
@@ -23,7 +10,6 @@ class kaplanMeier:
         jsonbody= []
         i=0
         while i<len(self.kmc):
-
             test = {
                 "t": self.kmc[i][0],
                 "d": self.kmc[i][1],
@@ -38,27 +24,25 @@ class kaplanMeier:
     # reading from a single JSON
     def readFromJson(self,Json):
 
-        data = json.loads(json.dumps(Json))
-
+        data = Json
         vitalstatus = data["vitalStatus"]
         # calculate number of days
-        dateOfDiagnosis = datetime.strptime(str(data["dateOfDiagnosis"]), '%Y%m%d')
-        dateOfLastContact = datetime.strptime(str(data["dateOfLastContact"]), '%Y%m%d')
-
-        t = dateOfLastContact-dateOfDiagnosis
+        t = data['lengthOfFollowup']
+        if t < 0:
+            t = 0
 
         # performing linear search everytime
         if(len(self.kmc)==0):
             if (vitalstatus == 0):
-                self.kmc.append([t.days , 1, 0])
+                self.kmc.append([t , 1, 0])
             else:
-                self.kmc.append([t.days, 0, -1])
+                self.kmc.append([t, 0, -1])
 
 
         else:
             i = 0
             while i < len(self.kmc):
-                if (self.kmc[i][0] == t.days):
+                if (self.kmc[i][0] == t):
                     if (vitalstatus == 0):
                         self.kmc[i][1] += 1
                     else:
@@ -69,27 +53,15 @@ class kaplanMeier:
 
             if (i==len(self.kmc)):
                 if (vitalstatus == 0):
-                    self.kmc.append([t.days, 1, 0])
+                    self.kmc.append([t, 1, 0])
                 else:
-                    self.kmc.append([t.days, 0, -1])
+                    self.kmc.append([t, 0, -1])
 
-        """
-        #sample data
-        self.kmc.append([1, 2, 0])
-        self.kmc.append([20, 0, 0])
-        self.kmc.append([8, 1, -1])
-        self.kmc.append([400, 0, 0])
-        self.kmc.append([974, 1, 0])
-        self.kmc.append([874, 0, -1])
-        """
         return self.kmc
 
 
     def buildKaplanMeier(self, totalpopulation):
         self.kmc.sort()
-
-
-
         self.kmc[0][2] = totalpopulation - self.kmc[0][1] + self.kmc[0][2]
         #print(0, " ", self.kmc[0][0], "-", self.kmc[0][1], "-", self.kmc[0][2])
         i = 1
@@ -104,14 +76,16 @@ class kaplanMeier:
 
 
 
-
-
-km=kaplanMeier()
-a = []
-i=0
-while i<len(jsonstring):
-    a+=km.readFromJson(jsonstring[i])
-    i+=1
-
-km.buildKaplanMeier(len(jsonstring))
-print(km.buildJson())
+if __name__ == "__main__":
+    jsonstring =[
+        { "_id" : "5b1d4fd78e5651446c445449", "patientIdNumber" : 935, "dateOfDiagnosis" : 20110710, "addrAtDxState" : "AR", "derivedAjcc6StageGrp" : "99", "sex" : "2", "race1" : "99", "dateOfLastContact" : 20161104, "lengthOfFollowup" : 1944, "primarySite" : "C51", "ageAtDiagnosis" : 89, "derivedAjcc7StageGrp" : 999, "vitalStatus" : "1" },
+        { "_id" : "5b1d4fd78e5651446c4454c7", "patientIdNumber" : 1061, "dateOfDiagnosis" : 20110318, "addrAtDxState" : "MO", "derivedAjcc6StageGrp" : "01", "lengthOfFollowup" : 0, "sex" : "1", "race1" : "99", "dateOfLastContact" : 20110318, "majorStageGrp" : "0", "primarySite" : "C67", "ageAtDiagnosis" : 50, "derivedAjcc7StageGrp" : 10, "vitalStatus" : "1" },
+        { "_id" : "5b1d4fd78e5651446c4455ce", "patientIdNumber" : 1324, "dateOfDiagnosis" : 20120130, "addrAtDxState" : "TX", "derivedAjcc6StageGrp" : "01", "lengthOfFollowup" : 0, "sex" : "1", "race1" : "96", "dateOfLastContact" : 20120130, "majorStageGrp" : "0", "primarySite" : "C67", "ageAtDiagnosis" : 72, "derivedAjcc7StageGrp" : 10, "vitalStatus" : "1" },
+        { "_id" : "5b1d4fd88e5651446c445634", "patientIdNumber" : 1426, "dateOfDiagnosis" : 20100306, "addrAtDxState" : "NY", "derivedAjcc6StageGrp" : "01", "lengthOfFollowup" : 0, "sex" : "2", "race1" : "99", "dateOfLastContact" : 20100306, "majorStageGrp" : "0", "primarySite" : "C67", "ageAtDiagnosis" : 70, "derivedAjcc7StageGrp" : 10, "vitalStatus" : "1" }
+    ]
+    km=kaplan_meier()
+    a = []
+    for record in jsonstring:
+        a+=km.readFromJson(record)
+    km.buildKaplanMeier(len(jsonstring))
+    print(km.buildJson())
